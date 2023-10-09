b85d0e8cdc2a2d78eacd35d55e1b8dfc311c87b4
jefri_widianto <jefri.widianto.ext@danamas.co.id>
2023-10-09 14:30:02 +0700

    - add github pipelines action - add API documentation with swagger open API

--- server.js
+++ server.js
@@ -19,12 +19,19 @@
         app.use(compression({ level: 9 }));
         app.use(cors());
 
+        //Init for all Environment & connect to all DB ENGINE
         let conMongo = await connectMongo.connectMongo();
         global.rootPath = __dirname;
         global.$hostPath = process.env.HOST_PATH;
         console.log(conMongo);
 
+        // Init for swagger API Doc.
+        const swaggerUi = require('swagger-ui-express');
+        const openApiDocumentation = require('./openApiDocumentation/openApiDocumentation.json');
+
+        //Collected All Routing API
         app.use('/tested', require('./src/tested'));
+        app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDocumentation));
 
         http.listen(process.env.NODE_SERVER_PORT, () => {
             console.log(`Successfully load config on => ${process.env.NODE_ENV}`)
